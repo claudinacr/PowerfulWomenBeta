e495ca856e07e66bf70df16c91fbb1ea19742e68
Claudina Cortez <claudinacr@gmail.com>
2018-01-05 11:58:31 -0300

    Implementacion

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,27 +12,27 @@ var projects = {
             'active': true,
             'message': 'Feliz inicio de proyecto',
             'photoSt': 'assets/images/girl2.png',
-            'newFeed': 'Estoy...%',
+            'newFeed': 'Estoy iniciando este nuevo proyecto. Éxito a todas',
           },
           {
             'name': 'Maria',
             'active': true,
             'message': 'Feliz inicio de proyecto',
             'photoSt': 'assets/images/girl5.png',
-            'newFeed': 'Estoy...%',
+            'newFeed': 'Estoy muy motivada. Quiero hacer crecer mi portafolio. ',
           }
         ],
         'description': [{
-          'information': 'Info',
+          'information': 'Este proyecto tiene como finalidad hacer una página web para un negocio de comida rápida.',
           'time': '5 days',
         }],
         'article': [{
-          'title': 'Donna Sloper',
-          'content': 'Este articulo...',
-          'author': 'Ja',
+          'title': 'Secretos de Web para negocios',
+          'content': 'Este articulo te enseñará las técnicas para hacer una página web ajustada a las exigencias de los negocios de comida rápida',
+          'author': 'Watts Wont',
         }]
       },
-      'web': {
+      'Portal Web Propio': {
         'teacher': [{
           'name': 'Carey Mary',
           'photo': 'assets/images/teacher11.png',
@@ -40,90 +40,96 @@ var projects = {
         }],
         'students': [{
           'name': 'Donna Sloper',
-          'photo': 'assets/images/girl14.png',
           'active': true,
           'message': 'Feliz inicio de proyecto',
-          'newFeed': 'Estoy...%',
+          'photoSt': 'assets/images/girl14.png',
+          'newFeed': 'Estoy muy contenta de iniciar el año con este proyecto. ',
 
 
         }],
         'description': [{
-          'information': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'time': '5 days',
+          'information': 'Este proyecto tiene como finalidad crear un portal web personal',
+          'time': '10 days',
         }],
         'article': [{
-          'title': 'Donna Sloper',
-          'content': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'author': true,
+          'title': 'Portales Webs Personales',
+          'content': '¿Necesitas crear tu propio portal web?. Aquí te enseñamos cómo',
+          'author': 'Antony Ralph',
         }]
       }
     },
     'Desarrollo de Videojuegos': {
       'maze': {
         'teacher': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Game Gates',
+          'photo': 'assets/images/girl2.png',
           'active': true,
         }],
         'students': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Gary Tack',
           'active': true,
+          'message': 'Feliz inicio de proyecto',
+          'photoSt': 'assets/images/girl7.png',
+          'newFeed': 'Primera vez que hago un video juego. Lo desarrollaré lo mejor posible. ',
         }],
         'description': [{
-          'information': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'time': '5 days',
+          'information': 'El proyecto consiste en crear un laberinto. El diseño es libre.',
+          'time': '15 days',
         }],
         'article': [{
-          'title': 'Donna Sloper',
-          'content': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'author': true,
+          'title': 'Nociones básicas de Videojuegos',
+          'content': 'En este artículo hablaremos sobre los aspectos fundamentales a tener en cuenta al momento de desarrollar un videojuego.',
+          'author': 'Gustavo León',
         }]
       }
     },
     'Desarrollo Mobile': {
       'jsConsole': {
         'teacher': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Consaly Jazz',
+          'photo': 'assets/images/girl10.png',
           'active': true,
         }],
         'students': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Nataly Jam',
           'active': true,
+          'message': 'Feliz inicio de proyecto',
+          'photoSt': 'assets/images/girl11.png',
+          'newFeed': 'Un reto muy interesante. Prondré lo mejor de mi para este desarrollo. ',
         }],
         'description': [{
-          'information': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'time': '5 days',
+          'information': 'La finalidad de este reto consiste en crear una consola que interprete javaScript.',
+          'time': '20 days',
         }],
         'article': [{
-          'title': 'Donna Sloper',
-          'content': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'author': true,
+          'title': 'Jack Cals',
+          'content': '¿Cómo crear una consola? Estas a punto de adentrarte en este tipo de desarrollo...',
+          'author': 'Gall Funny',
         }]
       }
     },
     'Aplicaciones de Escritorio': {
       'OpenOffice': {
         'teacher': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Deskman Catherine Scott',
+          'photo': 'assets/images/girl3.png',
           'active': true,
         }],
         'students': [{
-          'name': 'Donna Sloper',
-          'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+          'name': 'Carline Ramirez',
           'active': true,
+          'message': 'Feliz inicio de proyecto',
+          'photoSt': 'assets/images/girl16.png',
+          'newFeed': 'Antes había usado software para crear documentos. Ahora voy a crear la app con que hago los documentos. Muy divertido. ',
         }],
         'description': [{
-          'information': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'time': '5 days',
+          'information': 'La finalidad de este proyecto es crear un software para crear documentos. ',
+          'time': '14 days',
         }],
         'article': [{
-          'title': 'Donna Sloper',
-          'content': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
-          'author': true,
+          'title': 'Cuando pasas de ser un usuario de un paquete ofimático a desarrollador de uno',
+          'content': 'Pasar de ser un usuario de una app a ser desarrollado de una app, dos cosas distintas...',
+          'author': 'Nancy Ramirez',
         }]
       }
     }
